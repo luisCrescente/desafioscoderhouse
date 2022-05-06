e2bcdb9a17310a045ababa36aed953ce38fe162f
kany west <luis-94@live.com>
2022-05-06 19:32:26 -0300

    segundo punto del desafio terminado  2/2

--- desafio.js
+++ desafio.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
-const fs = require('fs'); 
 const port = 8080;
+const fs = require('fs'); 
 const products = require('./products');
+const allProducts = 'productos.txt';
+
 
 app.get('/products', (req, res, next )=>{
     res.send(products);
@@ -10,8 +12,23 @@ app.get('/products', (req, res, next )=>{
 
 
 
-app.get('/productRandom', (req, res, next )=>{
-    res.send();
+app.get('/productsRandom', (req, res, next )=>{
+    try {
+        fs.readFile(allProducts,"utf-8", (error, object) => {
+            const getAllProducts = JSON.parse(object);
+
+            const max=getAllProducts[getAllProducts.length-1].id;
+            const random = (Math.floor(Math.random()*max))+1;
+
+            for (let i=0; i < getAllProducts.length; i++) {
+                
+                if(random==getAllProducts[i].id){
+                    res.send(getAllProducts[i]);
+                }
+            } 
+        });
+    } catch (err){ console.log(err);}
+    
 });
 
 app.listen(port,()=>{
